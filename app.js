46ae762a8f379cee01da821cb0c85a93c7a94f4d
fed-gren <deveb22@gmail.com>
2019-07-09 17:03:37 +0900

    style: skeleton code

---
+++ app.js
@@ -0,0 +1,7 @@
+/**
+ * app에서 start page를 렌더링하기 위해 필요한 초기화 작업을 한다.
+ * StartController, StartView, ChampionModel
+ */
+const startController = new StartController();
+const startView = new StartView();
+const championModel = new ChampionModel();
---
+++ components/StartView.js
@@ -0,0 +1,15 @@
+/**
+ * description: 첫 화면 UI
+ * render 함수가 실행되고 나면 전체 페이지 보여줌.
+ */
+
+class StartView {
+  constructor() {
+    //DOM cacheing
+  }
+
+  render(data) {
+    //데이터 받아와서 템플릿 완성 시키고 mainContents DOM에 넣고
+    //화면 보여주기.
+  }
+}
