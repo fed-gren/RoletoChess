215cf653820062dea4d3a7bd2fada2bc3d2a7d65
fed-gren <deveb22@gmail.com>
2019-07-12 01:34:54 +0900

    fix: css load at html

--- components/ClassSelectView.js
+++ components/ClassSelectView.js
@@ -3,44 +3,21 @@ import templates from "../templates/index.js";
 
 export default class ClassSelectView {
   constructor() {
-    this.loadCardCss();
-    this.loadCardSelectCss();
     this.render();
-  }
-
-  loadCardCss() {
-    // Get HTML head element
-    const head = document.getElementsByTagName("HEAD")[0];
-
-    // Create new link Element
-    const link = document.createElement("link");
-
-    // set the attributes for link element
-    link.rel = "stylesheet";
-    link.type = "text/css";
-    link.href = "/components/styles/card.css";
-
-    // Append link element to HTML head
-    head.appendChild(link);
-  }
-
-  loadCardSelectCss() {
-    // Get HTML head element
-    const head = document.getElementsByTagName("HEAD")[0];
-
-    // Create new link Element
-    const link = document.createElement("link");
-
-    // set the attributes for link element
-    link.rel = "stylesheet";
-    link.type = "text/css";
-    link.href = "/components/styles/cardSelect.css";
-
-    // Append link element to HTML head
-    head.appendChild(link);
   }
 
   render() {
     this.template = templates.startClassSelectView(Model.classes);
+  }
+
+  attachEvent() {
+    this.classSelect = document.querySelector(".class-select");
+
+    this.classSelect.addEventListener("click", ({ target }) => {
+      if(Model.firstClass) return;
+
+      if (target.classList.contains("class-card"))
+        Model.firstClass = target.dataset.class;
+    });
   }
 }
