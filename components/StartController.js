f2d36055ee411a047632987c7ec0fb32243223a5
fed-gren <deveb22@gmail.com>
2019-07-12 02:40:01 +0900

    feat: fade effect
    - StartView에서 MapView로 렌더링 될 때 fade 효과 적용

--- components/StartController.js
+++ components/StartController.js
@@ -6,8 +6,25 @@ export default class StartController {
     this.classSelectView = this.startView.classSelectView;
     this.mapView = mapView;
 
+    this.container = document.querySelector(".roletoChess-container");
+    this.container.style.transition = `200ms opacity`;
+
     this.classSelectView.on("start", () => {
-      this.mapView.render();
+      this.mapViewRender();
     });
+  }
+
+  mapViewRender() {
+    // TODO: magic number 없애기
+    this.container.style.opacity = 0;
+    this.sleep(200).then(() => {
+      this.mapView.render();
+      this.container.style.opacity = 1;
+    });
+
+  }
+
+  sleep(time) {
+    return new Promise(resolve => setTimeout(resolve, time));
   }
 }
